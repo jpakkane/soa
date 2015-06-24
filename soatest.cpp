7407ffd3f6f9185876ea937dcfb04fde54fd3cc9
Jussi Pakkanen <jpakkane@gmail.com>
2015-06-24 23:46:45 +0300

    Removed dupe code.

--- soatest.cpp
+++ soatest.cpp
@@ -99,84 +99,11 @@ void sort_test() {
     assert(foo[3].item2 == 4);
 }
 
-void elem_test() {
-    std::vector<int> foobar{0, 1, 2, 3, 4};
-    ElementProxy<int> p1(foobar, 0);
-    ElementProxy<int> p2(foobar, 2);
-    assert(foobar[0] == 0);
-    assert(foobar[2] == 2);
-    assert(p1.value() == 0);
-    assert(p2.value() == 2);
-    std::swap(p1, p2);
-    assert(p1.value() == 2);
-    assert(p2.value() == 0);
-    assert(foobar[0] == 2);
-    assert(foobar[2] == 0);
-
-    p1 = 44;
-    assert(foobar[0] == 44);
-}
-
-void struct_test() {
-    std::vector<int> v1{0, 1, 2, 3, 4};
-    std::vector<int> v2{4, 3, 2, 1, 0};
-    ElementProxy<int> p1(v1, 0);
-    ElementProxy<int> p2(v1, 1);
-    ElementProxy<int> p3(v2, 0);
-    ElementProxy<int> p4(v2, 1);
-    StructProxy<int> sp1(p1, p3);
-    StructProxy<int> sp2(p2, p4);
-    print_vector(v1);
-    print_vector(v2);
-
-    printf("\n");
-    std::swap(sp1, sp2);
-    print_vector(v1);
-    print_vector(v2);
-    assert(v1[0] == 1);
-    assert(v2[0] == 3);
-    assert(v1[1] == 0);
-    assert(v2[1] == 4);
-    assert(p1.value() == 1);
-    assert(p3.value() == 3);
-
-    sp1.item1 = 44;
-    assert(p1.value() == 44);
-}
-
-void sort_test2() {
-    std::vector<int> v1{0, 1, 2, 3, 4};
-    std::vector<int> v2{4, 3, 2, 1, 0};
-    std::vector<StructProxy<int>> combined;
-    for(int i=0; i<v1.size(); i++) {
-        combined.emplace_back(ElementProxy<int>(v1, i), ElementProxy<int>(v2, i));
-    }
-    auto ordering = [](const StructProxy<int> &p1, const StructProxy<int> &p2) {
-        //printf("%d %d\n", p1.item2.value(), p2.item2.value());
-        return p1.item2.value() < p2.item2.value();
-    };
-    std::sort(combined.begin(), combined.end(), ordering);
-    assert(v1[0] == 4);
-    assert(v1[1] == 3);
-    assert(v1[2] == 2);
-    assert(v1[3] == 1);
-    assert(v1[4] == 0);
-
-    assert(v2[0] == 0);
-    assert(v2[1] == 1);
-    assert(v2[2] == 2);
-    assert(v2[3] == 3);
-    assert(v2[4] == 4);
-}
-
 int main(int, char **) {
     basic_test();
     indexing_test();
     iterator_test();
     find_test();
-    elem_test();
-    struct_test();
     swap_test();
     sort_test();
-    sort_test2();
 }
